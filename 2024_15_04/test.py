15a7af83570b10a3eb0de3d3bcc0030118208975
Rafael Smaniotto Bettinelli <164341787+RSBettinelli@users.noreply.github.com>
2024-04-15 11:24:04 -0300

    exemplo1

---
+++ 2024_15_04/test.py
@@ -0,0 +1,14 @@
+a = 21
+b = "19"
+c = 19
+
+print("Digite 3 numeros inteiros separados por ENTER para calcularmos a media")
+
+a = int(input("Primeiro numero: "))
+b = int(input("Segundo numero: "))
+c = int(input("Terceiro numero: "))
+
+media = (a + b + c) / 3
+print(f"A media e: {media}")
+media = round(media)
+print(f"A media arredondada e: {media}")
